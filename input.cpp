7273b7d9937bc1406bc187e6f57512a24e4fe50a
ArtSv <ArtSv@DESKTOP-JK44D29>
2024-05-13 22:26:07 +0300

    Добавьте файлы проекта.

---
+++ input.cpp
@@ -0,0 +1,26 @@
+﻿// input.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Требуется сложить два целых числа А и В.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int a, b;
+
+    cout << "Введите первое число: ";
+    cin >> a;
+
+    cout << "Введите второе число: ";
+    cin >> b;
+
+    cout << a + b << endl;
+
+
+    return 0;
+}
+
+
